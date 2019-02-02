7aee717e12c3e35bf067247e9001d351b34bf20e
yinm <yinm.cminor9@gmail.com>
2019-02-02 12:58:28 +0900

    Add 8.1.5

--- ch08/app/controllers/sessions_controller.rb
+++ ch08/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
-      flash[:danger] = 'invalid email/password combination'
+      flash.now[:danger] = 'invalid email/password combination'
       render 'new'
     end
   end
